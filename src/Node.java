4dfa007f1190dbc703fc04b618930e216e3dd00f
Haruyzal <racheeeve@gmail.com>
2024-03-13 17:48:43 +0100

    feat: Create empty constructor, and methods isEmpty() and add(Float, Node)

--- src/Node.java
+++ src/Node.java
@@ -13,12 +13,26 @@ public class Node {
 
     private GameState gameState;
 
+    Node() {
+    }
+
+    public boolean isEmpty() {
+        return nextNodes.isEmpty();
+    }
+
     public GameState getGameState() {
         return gameState;
     }
 
     public int getUtility() {
         return gameState.countTokens()[gameState.getPlayerInTurn()-1];
+    }
+
+    public void add(Float f, Node n) {
+        while (nextNodes.containsKey(f)) {
+            f += Float.MIN_VALUE;
+        }
+        nextNodes.put(f,n);
     }
 
 }
